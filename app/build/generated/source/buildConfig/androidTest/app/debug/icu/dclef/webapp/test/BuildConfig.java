714048f5732f27d71d95a5998a37935a21c0f7c5
rex <rex@wingame.com>
2025-03-07 22:16:13 +0800

    feat: 修复风险提示，修复logo白边“

--- app/build/generated/source/buildConfig/androidTest/app/debug/icu/dclef/webapp/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/app/debug/icu/dclef/webapp/test/BuildConfig.java
@@ -5,7 +5,7 @@ package icu.dclef.webapp.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "icu.dclef.webview.test";
+  public static final String APPLICATION_ID = "icu.dclef.webapp.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "app";
 }
--- app/build/generated/source/buildConfig/app/debug/icu/dclef/webapp/BuildConfig.java
+++ app/build/generated/source/buildConfig/app/debug/icu/dclef/webapp/BuildConfig.java
@@ -5,11 +5,9 @@ package icu.dclef.webapp;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "icu.dclef.webview";
+  public static final String APPLICATION_ID = "icu.dclef.webapp";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "app";
-  public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.0";
   // Field from product flavor: app
-  public static final String URL = "https://test.wgsaas.ai?al=41765741&ea=23&de=13&channel=apk&fb_dynamic_pixel=1181151730237356&fb_token=EAAIKaTaxxikBOZC1Efc9BmvjArxiY7knZBrlHQu1AFerbALDeT1avZBc45lMefwu1w6d8sG8nRHj7iEJrFqZBCwZAjwSZB7ZC189Wx7M3MBUtyCrXrb4NeXy1kGzLvJvkQQh8g2NkBDcEEPmHhnxeko71BU0haIGyGntksNekBDSZAr7wFCRWoNJNBYsZBIj75xmP7AZDZD";
+  public static final String URL = "https://www.luck7.game?al=42975099&ea=12&de=15&channel=apk&fb_dynamic_pixel=1181151730237356&fb_token=EAAIKaTaxxikBOZCltucrq2Tbu2iaqAVaURMZAqxsZAMnfLYYKlqgsbSGWvfz2XblbYHssDdEue0dd5uYe01bywGaI2KQNfxOpdzkWjYkuIoiVmAgfIQR3r7rrWRZCcllwanXGBF1GUNEAyYJtqYDv40EuLDZBL2ybvH2SBjQ9x9R9zPzq4sTGSX4jJrfnO4GNagZDZD";
 }
